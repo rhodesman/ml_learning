e315d56969c991b2b606323898cd71c6dbf65387
Jason Rhodes <jason.rhodes@gmail.com>
2024-12-20 15:04:27 -0500

    removing depricated code

--- csv_test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Load the processed stock data
-stock_data = pd.read_csv("data/processed/cleaned_stock_data.csv")
-
-# Display the first few rows and data types
-print(stock_data.head())
-print(stock_data.dtypes)
--- tendorFlow.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-
-import torch
-print("Is CUDA available: ", torch.cuda.is_available())
-print("CUDA Version: ", torch.version.cuda)
